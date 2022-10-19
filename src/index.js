86a13d181805e0da07374d334c9ed85716231555
aavv8931 <arturo_veintimilla@hotmail.com>
2022-10-19 00:26:04 -0500

    Add Carousel functionality.

---
+++ src/index.js
@@ -0,0 +1,47 @@
+const slides = document.getElementsByClassName("carousel-item");
+const nextButton = document.getElementById("carousel-button-next");
+const prevButton = document.getElementById("carousel-button-prev");
+let position = 0;
+const numberOfSlides = slides.length;
+
+
+function hideAllSlides() {
+    // remove all slides not currently being viewed
+    for (const slide of slides) {
+        slide.classList.remove("carousel-item-visible");
+        slide.classList.add("carousel-item-hidden");
+    }
+}
+
+const handleMoveToNextSlide = (event) => {
+    hideAllSlides();
+
+    // check if last slide has been reached
+    if (position === numberOfSlides - 1) {
+        position = 0; // go back to first slide
+    } else {
+        // move to next slide
+        position++;
+    }
+    // make current slide visible
+    slides[position].classList.add("carousel-item-visible");
+}
+
+const handleMoveToPrevSlide = (event) => {
+    hideAllSlides();
+
+    // check if we're on the first slide
+    if (position === 0) {
+        position = numberOfSlides - 1; // move to the last slide
+    } else {
+        // move back one
+        position--;
+    }
+    // make current slide visible
+    slides[position].classList.add("carousel-item-visible");
+
+}
+
+
+nextButton.addEventListener("click", handleMoveToNextSlide);
+prevButton.addEventListener("click", handleMoveToPrevSlide);
